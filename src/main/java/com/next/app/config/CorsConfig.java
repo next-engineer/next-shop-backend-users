207be212ac1a18330dd58948a81f0b89bad95308
gogogojun <rhwnsgh11@naver.com>
2025-08-18 21:52:41 +0900

    CorsConfig 문법오류 수정

--- src/main/java/com/next/app/config/CorsConfig.java
+++ src/main/java/com/next/app/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
                 "http://localhost:3000",
                 "http://localhost:5173",
                 //alb DNS 주소
-                "dong-test-alb-1868547901.ap-northeast-2.elb.amazonaws.com"
+                "dong-test-alb-1868547901.ap-northeast-2.elb.amazonaws.com",
                 "shop-user-alb-72649105.ap-northeast-2.elb.amazonaws.com"
                 //프론트엔드 도매인 생성 시 사용.
 //                "http://shop.nextcloudlab.com"
